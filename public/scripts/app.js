05c6666acf74f78bd3bfd131317cce1394366581
Danny Chan <Danny@Dannys-MacBook-Pro-2.local>
2017-05-04 11:34:42 -0700

    stable build, need testing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,52 +20,180 @@ function config($stateProvider, $urlRouterProvider, $locationProvider) {
               controller: 'HomeController',
               controllerAs: 'home'
             })
-
-
 }
 
-
 //controllers
-
 HomeController.$inject = ['$http']; //minification protection
 function HomeController ($http) {
   var vm = this;
-  console.log('home controller');
+  var key_wypnt = [];
+  var waypoint1;
+  var waypoint2;
+  var waypoint3;
 
   var directionsService = new google.maps.DirectionsService();
   var directionsDisplay;
+  var places;
   // map
    var map = new google.maps.Map(document.getElementById('map'), {
      zoom:13,
      center: {lat: 37.785636, lng: -122.397119},
      mapTypeId: google.maps.MapTypeId.ROADMAP
    });
+   //add places
+   places = new google.maps.places.PlacesService(map);
 
    $('input.mode').on('change', function() {
        $('input.mode').not(this).prop('checked', false);
    });
 
+   function doSearch() {
+     var keyword = document.getElementById('keyword').value;
+     //keyword 1
+    var type = document.getElementById('type').value;
+    var rankBy = document.getElementById('rankBy').value;
+    var search = {};
 
+    if (keyword) {
+      search.keyword = keyword;
+    }
+
+    if (type != 'establishment') {
+      search.types = [type];
+    }
+
+    if (rankBy == 'distance' && (search.types || search.keyword)) {
+      search.rankBy = google.maps.places.RankBy.DISTANCE;
+      search.location = map.getCenter();
+    } else {
+      search.bounds = map.getBounds();
+    }
+
+    places.search(search, function(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var lat = results[0].geometry.location.lat();
+      var lng = results[0].geometry.location.lng();
+      waypoint1 = {
+       location: {lat, lng},
+       stopover: true
+      }
+    }
+  });
+}
+
+function doSearch2() {
+  //keyword 2
+var keyword2 = document.getElementById('keyword2').value;
+ var type2 = document.getElementById('type2').value;
+ var rankBy = document.getElementById('rankBy').value;
+ var search2 = {};
+
+ if (keyword2) {
+   search2.keyword = keyword2;
+ }
+
+ if (type2 != 'establishment') {
+   search2.types = [type2];
+ }
+
+ if (rankBy == 'distance' && (search2.types || search2.keyword2)) {
+   search2.rankBy = google.maps.places.RankBy.DISTANCE;
+   search2.location = map.getCenter();
+ } else {
+   search2.bounds = map.getBounds();
+ }
+
+ places.search(search2, function(results, status) {
+ if (status == google.maps.places.PlacesServiceStatus.OK) {
+   var lat = results[0].geometry.location.lat();
+   var lng = results[0].geometry.location.lng();
+   waypoint2 = {
+    location: {lat, lng},
+    stopover: true
+   }
+ }
+});
+}
+
+
+function doSearch3() {
+  var keyword3 = document.getElementById('keyword3').value;
+  //keyword 3
+ var type3 = document.getElementById('type3').value;
+ var rankBy = document.getElementById('rankBy').value;
+ var search3 = {};
+
+ if (keyword3) {
+   search3.keyword = keyword3;
+ }
+
+ if (type3 != 'establishment') {
+   search3.types = [type3];
+ }
+
+ if (rankBy == 'distance' && (search3.types || search3.keyword3)) {
+   search3.rankBy = google.maps.places.RankBy.DISTANCE;
+   search3.location = map.getCenter();
+ } else {
+   search3.bounds = map.getBounds();
+ }
+
+ places.search(search3, function(results, status) {
+ if (status == google.maps.places.PlacesServiceStatus.OK) {
+   var lat = results[0].geometry.location.lat();
+   var lng = results[0].geometry.location.lng();
+   waypoint3 = {
+    location: {lat, lng},
+    stopover: true
+   }
+ }
+});
+}
+
+  $('#kw_submit').click(function(){
+    setTimeout(function(){
+      doSearch();
+      doSearch2();
+      doSearch3();
+    }, 5000);
+
+    $('#submit').show();
+    $('#kw_submit').hide();
+  });
 
 
    //show stopover input boxes
  $('#add_stop').click(function(){
-   $('#stop1').show();
-   $('#stop2').show();
-   $('#stop3').show();
+   if ($('#transit')){
+     $('#transit').prop("checked", false);
+
+     $('#transit').hide();
+     $('#t_label').hide();
+   }
    $('#add_stop').hide();
-   $('#no_stop').show();
+   $('#no_stop').removeClass('hide_btn');
+   $('.waypoints').show();
+   $('#kw_submit').removeClass('hide_btn');
+   $('#submit').hide();
+   $('.s_msg').show();
  });
+
  $('#no_stop').click(function(){
-   $('#stop1').hide();
-   $('#stop2').hide();
-   $('#stop3').hide();
    $('#add_stop').show();
-   $('#no_stop').hide();
+   $('#no_stop').addClass('hide_btn');
+   $('.waypoints').hide();
+   $('#kw_submit').addClass('hide_btn');
+   $('.s_msg').hide();
+   $('#transit').show();
+   $('#t_label').show();
  });
 
   $('#submit').click(function() {
-
+    var org = $('#address').val();
+    if (!org) {
+      alert('please enter origin address');
+      return;
+    }
     map = new google.maps.Map(document.getElementById('map'), {
       zoom:13,
       center: {lat: 37.785636, lng: -122.397119},
@@ -77,17 +205,25 @@ function HomeController ($http) {
       var address = $('#address').val();
       directionsDisplay.setMap(map);
       directionsDisplay.setPanel(document.getElementById('panel'));
-     var wypnt = [];
+      var wypnt = [];
          //check for waypoints
-         if ($('#waypoint_1').val()){
+         if (waypoint1){
+           wypnt.push(waypoint1);
+         } else if ($('#waypoint_1').val()) {
            wypnt.push({location: $('#waypoint_1').val(), stopover: true});
          }
-         if ($('#waypoint_2').val()) {
+         if (waypoint2) {
+           wypnt.push(waypoint2);
+         } else if ($('#waypoint_2').val()) {
            wypnt.push({location: $('#waypoint_2').val(), stopover: true});
          }
-         if ($("#waypoint_3").val()){
+         if (waypoint3){
+           wypnt.push(waypoint3);
+         } else if ($("#waypoint_3").val()) {
            wypnt.push({location: $("#waypoint_3").val(), stopover: true});
+
          }
+
      var tMOde;
      if($('#walking').is(":checked")){
        tMode = "WALKING";
@@ -110,8 +246,13 @@ function HomeController ($http) {
       directionsService.route(request, function(response, status) {
        if (status == google.maps.DirectionsStatus.OK) {
            directionsDisplay.setDirections(response);
+           waypoint1;
+           waypoint2;
+           waypoint3;
        }
       });
+
+
   });
 
 }
